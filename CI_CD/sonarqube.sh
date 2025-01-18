f726f0f091e8ecef420a6b8e7261ce51fa558634
satheeshr240 <satheeshr241@gmail.com>
2025-01-18 20:35:46 +0530

    Sonar

--- CI_CD/sonarqube.sh
+++ CI_CD/sonarqube.sh
@@ -27,30 +27,31 @@ sudo systemctl enable docker
 # Restart Docker Daemon 
 sudo systemctl restart docker
 
-# Download Sonarqube Image from hub.docker.com
+# Download a docker images of Sonarqube from hub.docker.com
 sudo docker pull sonarqube
 
-# display docker images
+# check Downloaded Docker Images of Sonarqube
 sudo docker images
 
-# Create docker volumes
+# Create docker volumes to store the Sonarqube persistent data
 docker volume create sonarqube-conf
 docker volume create sonarqube-data
 docker volume create sonarqube-logs
 docker volume create sonarqube-extensions
 
-# Checking docker volumes
+# Verify the persistent data directories
 #docker volume inspect sonarqube-conf
 #docker volume inspect sonarqube-data
 #docker volume inspect sonarqube-logs
 #docker volume inspect sonarqube-extensions
 
-# Linking docker volumes
+# create directory  
 sudo mkdir /sonarqube
+# Optionally, create symbolic links to an easier access location 
 ln -s /var/lib/docker/volumes/sonarqube-conf/_data /sonarqube/conf
 ln -s /var/lib/docker/volumes/sonarqube-data/_data /sonarqube/data
 ln -s /var/lib/docker/volumes/sonarqube-logs/_data /sonarqube/logs
 ln -s /var/lib/docker/volumes/sonarqube-extensions/_data /sonarqube/extensions
 
-# Create Sonarqube container
+# Start a Sonarqube container with persistent data storage 
 docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 -v sonarqube-conf:/sonarqube/conf -v sonarqube-data:/sonarqube/data -v sonarqube-logs:/sonarqube/logs -v sonarqube-extensions:/sonarqube/extensions sonarqube
